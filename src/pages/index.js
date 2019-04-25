b7992a588bbbab94c957a8ea325ee47742690478
Ramya <Ramya@Venkatas-MacBook-Pro.local>
2019-04-25 12:02:23 -0500

    sample form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <div>
+<form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <div data-netlify-recaptcha="true"></div>
+  <p>
+    <button type="submit" to="/thankPage/">Send</button>
+  </p>
+</form>
+</div>
+
---
+++ src/pages/thankYouPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const SecondPage = () => (
+  <div>
+    <h1>Thank you for your submission</h1>
+  </div>
+)
+
+export default SecondPage
